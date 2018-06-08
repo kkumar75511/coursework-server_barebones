953ce0986d936f0bf8576ee068f032d12d12efaa
Kyle <kkumar.75511@farnborough.ac.uk>
2018-06-08 14:20:49 +0100

    Commit #1

---
+++ src/server/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package server.controllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class HelloController {
+
+    @GET
+    @Path("hello")
+    @Produces(MediaType.TEXT_HTML)
+
+    public String helloWorld () {
+
+        return "<html><body><p>Hello World!</p></body></html>";
+
+    }
+
+}
