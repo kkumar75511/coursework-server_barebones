aaf5cd3151b17decc573793914c23d0a4eae4945
Kyle <kkumar.75511@farnborough.ac.uk>
2018-06-15 14:29:41 +0100

    Show messages in html
    Using ajax, I have created a loading fuction that get's the json message list and creates html code from each message. I also created another function that converts each message into html code each with their own div tags. The JSON is converted into styled HTML.

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,11 +1,41 @@
-function pageLoad () {
+function pageLoad() {
 
-    loadMessages()
+    loadMessages();
 
 }
 
 function loadMessages () {
 
-    alert('hello');
+    let messagesHTML = '';
+
+    $.ajax ({
+
+        url: '/message/list',
+        type: 'GET',
+        success: messageList => {
+
+            for (let message of messageList) {
+
+                messagesHTML += renderMessage(message);
+
+            }
+
+            $('#messages').html(messagesHTML);
+
+        }
+
+    })
+
+}
+
+function renderMessage (message) {
+
+    return  `<div class="border border-primary p-2 m-2">` +
+                `<div>` +
+                    `<span class="badge badge-primary mr-2">${message.author}</span>` +
+                    `<span class="badge badge-info">${message.postDate}</span>` +
+                `</div>` +
+                `<div class="py-2 mx-2">${message.text}</div>` +
+            `</div>`;
 
 }
