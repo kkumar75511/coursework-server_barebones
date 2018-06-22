d63651e079cd84f91b9594f69d9c3fab1dd43f6c
Kyle <kkumar.75511@farnborough.ac.uk>
2018-06-22 13:39:18 +0100

    Make a user model class

---
+++ src/server/models/User.java
@@ -0,0 +1,68 @@
+package server.models;
+
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String sessionToken;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSessionToken() {
+        return sessionToken;
+    }
+
+    public void setSessionToken(String sessionToken) {
+        this.sessionToken = sessionToken;
+    }
+
+    public static ArrayList<User> users = new ArrayList<>();
+
+    public static int nextId() {
+        int id = 0;
+        for (User u : users) {
+            if (u.getId() > id) {
+                id = u.getId();
+            }
+        }
+        return id + 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public JSONObject toJSON() {
+        JSONObject j = new JSONObject();
+        j.put("id", getId());
+        j.put("username", getUsername());
+        j.put("password", getPassword());
+        j.put("sessionToken", getSessionToken());
+
+
+        return j;
+    }
+}
