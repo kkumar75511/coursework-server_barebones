2989f98dfa74ad2cbe0b6552836574ac654b9e76
Kyle <kkumar.75511@farnborough.ac.uk>
2018-07-12 22:50:10 +0100

    Revert "Add trianglify to login"
    This reverts commit 8437786d25b951fce48c27d1d078184f2dd370bf.

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -49,18 +49,4 @@ function checkLogin () {
 function pageLoad() {
     resetLoginForm();
     resetNewUserForm();
-
-    const pattern = Trianglify({
-        width: window.innerWidth,
-        height: window.innerHeight
-    });
-
-    const firstDiv = $('div:first');
-    firstDiv.before(pattern.canvas);
-    firstDiv.addClass('fixed-top');
-
-    const body = $('body');
-    body.css('overflow-y', 'hidden');
-    body.css('overflow-x', 'hidden');
-
 }
